cc22a1f05b35eee4d2d9f63c97b80a624d22b085
xesam <xesamguo@gmail.com>
2018-07-25 12:02:55 +0800

    excel

--- proj_1/js/source/app.js
+++ proj_1/js/source/app.js
@@ -2,9 +2,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Logo from "./components/Logo";
+import Excel from "./components/Excel";
+
+var headers = localStorage.getItem('headers');
+var data = localStorage.getItem('data');
+if (!headers) {
+    headers = ['Title', 'Year', 'Rating', 'Comments'];
+    data = [['Test', '2015', '3', 'meh']];
+}
+
 ReactDOM.render(
-    <h1>
-        <Logo /> Welcome to The App!
-    </h1>,
+    <div>
+        <h1>
+            <Logo/> Welcome to The App!
+        </h1>
+        <Excel headers={headers} initialData={data}/>
+    </div>,
     document.getElementById("app")
 );
