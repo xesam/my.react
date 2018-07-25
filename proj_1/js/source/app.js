b6fd0dd833097a6d1be7aca71a069e306de3c852
xesam <xesamguo@gmail.com>
2018-07-25 12:44:40 +0800

    let

--- proj_1/js/source/app.js
+++ proj_1/js/source/app.js
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom";
 import Logo from "./components/Logo";
 import Excel from "./components/Excel";
 
-var headers = localStorage.getItem('headers');
-var data = localStorage.getItem('data');
+let headers = localStorage.getItem('headers');
+let data = localStorage.getItem('data');
 if (!headers) {
     headers = ['Title', 'Year', 'Rating', 'Comments'];
     data = [['Test', '2015', '3', 'meh']];
